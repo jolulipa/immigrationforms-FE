3e891ec38a91ab3ecae12c61b5ad0e7ebb4b9963
JL Liranzo <jolulipa@gmail.com>
2021-04-05 13:56:24 -0400

    crating tha UI

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import {
-    HashRouter as Router,
-    Switch,
-    Route,
-} from "react-router-dom";
-import Navigation from './components/navigation';
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Navigation from "./components/navigation";
 import I130 from "./forms/I130";
-import LandingPage from './screens/LandingPage';
+import I130A from "./forms/I130A";
+import LandingPage from "./screens/LandingPage";
 import N400 from "./forms/N400";
 
 function App() {
-    return <Router>
-        <Navigation/>
-        <Switch>
-            <Route path="/forms/i130">
-                <I130/>
-            </Route>
-            <Route path="/forms/n400">
-                <N400/>
-            </Route>
-            <Route path="/">
-                <LandingPage/>
-            </Route>
-        </Switch>
-    </Router>;
+  return (
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route path="/forms/i130">
+          <I130 />
+        </Route>
+        <Route path="/forms/i130A">
+          <I130A />
+        </Route>
+        <Route path="/forms/n400">
+          <N400 />
+        </Route>
+        <Route path="/">
+          <LandingPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
