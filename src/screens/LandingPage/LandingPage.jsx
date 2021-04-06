6a7b4db60f5aa1ddc8c8f2b3201b05db343e84b2
JL Liranzo <jolulipa@gmail.com>
2021-04-06 14:23:26 -0400

    modifying the landing page

--- src/screens/LandingPage/LandingPage.jsx
+++ src/screens/LandingPage/LandingPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./styles.css";
+const fs = require("fs");
+const formsDb = fs.readFileSync("./forms/formsDb.json", "utf8");
+const forms = JSON.parse(formsDb);
 
 const LandingPage = () => {
-  return (
-    <div>Este es el lugar en donde reside el landing page</div>
-  );
+  return <div>Este es el lugar en donde reside el landing page</div>;
 };
 
 export default LandingPage;
