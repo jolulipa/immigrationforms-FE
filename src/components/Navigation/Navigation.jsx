3cd95b4c9be196e220af81a554bbaa7d3e7368b1
JL Liranzo <jolulipa@gmail.com>
2021-04-08 13:33:20 -0400

    GUI coding

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,16 +1,33 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 const Navigation = () => (
-    <div className="super-root">
-        <div className="line">Texto de prueba</div>
-        <div className="root">
-            <div>Un texto cualquiera</div>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-        </div>
+  <div className="super-root">
+    <div className="line-container">
+      <div className="line-1">The Immigration Time</div>
+      <div className="line-2">
+        Nuestro Tel. 24/7
+        <div>+1-212-787-8777</div>
+      </div>
+      <div className="line-2">
+        E-mail
+        <div>john@gmail.com</div>
+      </div>
+      <div className="line-2">
+        Location
+        <div>New York, USA</div>
+      </div>
     </div>
+    <div className="root">
+      <div>Registrarse/Log in</div>
+      <Link to="/">HOME</Link>
+      <Link to="/about">ABOUT</Link>
+      <Link to="/team">TEAM</Link>
+      <Link to="/contact">CONTACT</Link>
+      <Link to="/login">LOGIN</Link>
+    </div>
+  </div>
 );
 
 export default Navigation;
