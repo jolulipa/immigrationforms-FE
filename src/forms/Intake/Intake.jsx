ad864c94378ae9d93c43af35da6d241ff213c9c6
Eliaquín Encarnación <eliaquin@gmail.com>
2021-06-08 20:50:44 -0400

    Fixed front end

--- src/forms/Intake/Intake.jsx
+++ src/forms/Intake/Intake.jsx
@@ -2,25 +2,18 @@ import React from "react";
 import Form from "@rjsf/bootstrap-4";
 import schema from "./Intakeschema";
 import uiSchema from "./IntakeUiSchema";
-import { createForm } from "../../api/formsAccess";
+import {createForm} from "../../api/formsAccess";
 
 const Intake = () => {
-  const extractData = ({ formData }) => {
-    let i;
-    for (i = 1; i < 100; i++) {
-      const aVolar = `text${i}`;
-      delete formData.p1[aVolar];
-      delete formData.p2[aVolar];
-    }
+    const extractData = async ({formData}) => {
+        const obj = {data: JSON.stringify(formData), formId: "Intake"};
+        await createForm(obj);
+    };
 
-    const obj = { data: formData, formId: "Intake" };
-    createForm(obj);
-  };
-
-  return (
-    <div className="container">
-      <Form schema={schema} uiSchema={uiSchema} onSubmit={extractData} />
-    </div>
-  );
+    return (
+        <div className="container">
+            <Form schema={schema} uiSchema={uiSchema} onSubmit={extractData}/>
+        </div>
+    );
 };
 export default Intake;
