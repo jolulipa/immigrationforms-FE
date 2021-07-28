70158ec088422afc56ee410bf0db505b5dc11bdd
JL Liranzo <jolulipa@gmail.com>
2021-07-28 15:06:14 -0400

    fix navigation problem

--- src/forms/Intake/Intake.jsx
+++ src/forms/Intake/Intake.jsx
@@ -29,6 +29,12 @@ const Intake = () => {
       const aVolar = `text${i}`;
       delete formData.p1[aVolar];
       delete formData.p2[aVolar];
+      delete formData.p3[aVolar];
+      delete formData.p4[aVolar];
+      delete formData.p5[aVolar];
+      delete formData.p6[aVolar];
+      delete formData.p7[aVolar];
+      delete formData.p8[aVolar];
     }
   };
 
