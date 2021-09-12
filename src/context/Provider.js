4896e34a325aa5e48fafb72ff92796deb95865af
jolulipa <79167045+jolulipa@users.noreply.github.com>
2021-09-11 23:38:11 -0400

    solving one conflict

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createContext, useContext, useReducer } from "react";
 import { EMAIL_TYPE, INTAKE_TYPE } from "./types";
 
@@ -25,34 +24,6 @@ const reducer = (state, action) => {
           ...action.payload,
         },
       };
-=======
-import {createContext, useContext, useReducer} from 'react';
-import {EMAIL_TYPE, INTAKE_TYPE} from "./types";
-
-const initialState = {
-    email: localStorage.getItem(EMAIL_TYPE) || '',
-    intake: localStorage.getItem(INTAKE_TYPE) ? JSON.parse(localStorage.getItem(INTAKE_TYPE)) : {
-        firstName: '',
-        middleName: '',
-        lastName: '',
-        fullName: '',
-    }
-}
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case EMAIL_TYPE:
-            return {...state, email: action.payload}
-        case INTAKE_TYPE: {
-            return {
-                ...state, intake: {
-                    ...action.payload
-                }
-            }
-        }
-        default:
-            throw new Error('Invalid option passed to reducer');
->>>>>>> e8c668dbe694c9a7dabdc3ee641df2a53bff5214
     }
     default:
       throw new Error("Invalid option passed to reducer");
@@ -63,7 +34,6 @@ const AppContext = createContext(initialState);
 
 export const useAppContext = () => useContext(AppContext);
 
-<<<<<<< HEAD
 const AppContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const updateEmail = (email) => {
@@ -77,20 +47,6 @@ const AppContextProvider = ({ children }) => {
   return (
     <AppContext.Provider value={{ state, updateEmail, updateIntake }}>
       {children}
-=======
-const AppContextProvider = ({children}) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const updateEmail = (email) => {
-        localStorage.setItem(EMAIL_TYPE, email);
-        dispatch({type: EMAIL_TYPE, payload: email});
-    }
-    const updateIntake = (intakeObj) => {
-        localStorage.setItem(INTAKE_TYPE, JSON.stringify(intakeObj));
-        dispatch({type: INTAKE_TYPE, payload: intakeObj});
-    }
-    return <AppContext.Provider value={{state, updateEmail, updateIntake}}>
-        {children}
->>>>>>> e8c668dbe694c9a7dabdc3ee641df2a53bff5214
     </AppContext.Provider>
   );
 };
